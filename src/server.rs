9627e29040771330fba4a6703160d6de132f02a9
Gerald <gombirog@gmail.com>
2023-11-21 19:25:35 +0200

    extraction of method, path & method from request

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,8 @@
 use std::net::{TcpListener, TcpStream};
+use std::io::Read;
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::convert::TryInto;
 
 pub struct Server {
     address: String,
@@ -15,12 +19,21 @@ impl Server {
         let listener = TcpListener::bind(&self.address).unwrap();
 
         loop{
-            let result = listener.accept();
-
-            match result{
+            match listener.accept(){
                 Err(e) =>  println!("Failed to establish a connection: {}", e),
-                Ok((stream, _)) => {
-                    println!("Ok");
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer){
+                        Ok(_) => {
+                            println!("Received a request : {}", String::from_utf8_lossy(&buffer));
+
+                            match Request::try_from(&buffer[..]){
+                                Ok(request) => {},
+                                Err(e) => println!("Failed to parse request {}", e),
+                            }
+                        },
+                        Err(e) => println!("Failed to read from connection: {}", e),
+                    }
                 },
             }
         }
