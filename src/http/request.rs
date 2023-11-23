afe034158fc30d9f6f2abecbc3bc23157a9fff64
Gerald <gombirog@gmail.com>
2023-11-23 18:02:03 +0200

    representing the query string using a hash map

--- src/http/request.rs
+++ src/http/request.rs
@@ -4,11 +4,12 @@ use std::error::Error;
 use std::fmt::{Display, Formatter, Result as FmtResult, Debug};
 use std::str;
 use std::str::Utf8Error;
+use super::{QueryString, QueryStringValue};
 
 pub struct Request<'buf> {
     method: Method,
     path: &'buf str,
-    query_string: Option<&'buf str>,
+    query_string: Option<QueryString<'buf>>,
 }
 
 impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
@@ -29,7 +30,7 @@ impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
 
         let mut query_string = None;
         if let Some(i) = path.find('?')  {
-            query_string = Some(&path[i + 1..]);
+            query_string = Some(QueryString::from(&path[i + 1..]));
             path = &path[..i];
         }
 
