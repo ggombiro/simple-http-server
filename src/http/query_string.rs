405b658b3f506c001c95f9d89d09faa27f00c4c4
Gerald <gombirog@gmail.com>
2023-11-24 16:56:21 +0200

    remove unused variable vev in query_string

--- src/http/query_string.rs
+++ src/http/query_string.rs
@@ -33,7 +33,6 @@ impl<'buf> From<&'buf str> for QueryString<'buf>{
             data.entry(key)
             .and_modify(|existing: &mut Value| match existing {
                 Value::Single(prev_val) => {
-                    let mut vec = vec![prev_val, val];
                     *existing = Value::Multiple(vec![prev_val, val]);
                 },
                 Value::Multiple(vec) => vec.push(val),
